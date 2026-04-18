2c27518478ed15b8845a1f34728e17f5e95f58ba
NguetchuissiBrunel <landrybrunel5@gmail.com>
2026-04-18 08:19:21 +0100

    feat: configure Flyway database migration and application properties

---
+++ src/main/java/com/yowpainter/config/FlywayConfig.java
@@ -0,0 +1,29 @@
+package com.yowpainter.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class FlywayConfig {
+
+    private final DataSource dataSource;
+
+    public FlywayConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean(initMethod = "migrate")
+    public Flyway flyway() {
+        return Flyway.configure()
+                .dataSource(dataSource)
+                .schemas("public")
+                .locations("classpath:db/migration/public")
+                .baselineOnMigrate(true)
+                .outOfOrder(true)
+                .repairOnMigrate(true)
+                .load();
+    }
+}
